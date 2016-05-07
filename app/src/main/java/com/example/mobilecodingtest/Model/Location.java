13ca4bf4b5ef3995f9d9308903a3bf3fd832895e
diegoTabares32 <diego.e.tabares@gmail.com>
2016-05-07 16:36:32 -0300

    Adding model objects parsed from service

---
+++ app/src/main/java/com/example/mobilecodingtest/Model/Location.java
@@ -0,0 +1,16 @@
+package com.example.mobilecodingtest.Model;
+
+/**
+ * Created by Diego on 07/05/2016.
+ */
+public class Location {
+
+    private float latitude;
+    private float longitude;
+
+    //empty constructor for Gson
+    public Location(){
+        super();
+    }
+
+}
---
+++ app/src/main/java/com/example/mobilecodingtest/Model/WeaponsLocation.java
@@ -0,0 +1,18 @@
+package com.example.mobilecodingtest.Model;
+
+/**
+ * Created by Diego on 07/05/2016.
+ */
+public class WeaponsLocation {
+
+    private Location location;
+    private int radius;
+    private String code;
+    private int radiusInMeter;
+
+    //empty constructor for Gson
+    public WeaponsLocation(){
+        super();
+    }
+
+}
